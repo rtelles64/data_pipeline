6e3a69d6d0e4c9a2d3fc078aea0f585dc950f87f
rtelles64 <rtelles64@gmail.com>
2020-03-29 21:03:14 -0700

    Add implementation for quality checks

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -2,6 +2,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class DataQualityOperator(BaseOperator):
 
     ui_color = '#89DA59'
@@ -11,12 +12,32 @@ class DataQualityOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 test_query="",
+                 expected_result="",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.test_query = test_query
+        self.expected_result = expected_result
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        # self.log.info('DataQualityOperator not implemented yet')
+
+        self.log.info("Obtaining credentials")
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Running quality check")
+        records = redshift_hook.get_records(self.test_query)
+
+        if records[0][0] != self.expected_result:
+            raise ValueError(f"""
+                Data quality check failed:
+                    {records[0][0]} does not equal {self.expected_result}
+            """)
+        else:
+            self.log.info("Quality check passed")
