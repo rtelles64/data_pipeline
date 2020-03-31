8ff6ccdc5b7e7efc9c8a0a531deeacea47386c9e
rtelles64 <rtelles64@gmail.com>
2020-03-31 12:01:09 -0700

    Implement StageToRedshiftOperator

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,26 +1,66 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook  # easy connect to AWS
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+
+    copy_qry = """
+        COPY {} FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        REGION AS '{}'
+        FORMAT AS JSON '{}'
+    """
 
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
                  # Example:
                  # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 copy_json_option="auto",
+                 region="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials = aws_credentials
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.copy_json_option = copy_json_option
+        self.region = region
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        # self.log.info('StageToRedshiftOperator not implemented yet')
 
+        self.log.info("Getting credentials")
+        aws_hook = AwsHook(self.aws_credentials)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
+        self.log.info("Clearing data from destination table")
+        redshift.run(f"DELETE FROM {self.table};")
 
+        self.log.info("Copying data from S3 to Redshift")
+        rendered_key = self.s3_key.format(**context)
+        s3_path = f"s3://{self.s3_bucket}/{rendered_key}"
+        copy_sql = StageToRedshiftOperator.copy_qry.format(
+            self.table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            self.region,
+            self.copy_json_option
+        )
 
-
+        redshift.run(copy_sql)
